a2bb2e560903e55f98de4b904f5c1c910059040a
asthaverma-17 <av.asthaverma17@gmail.com>
2023-05-02 17:29:15 +0530

    assignment1Q7 is modified

--- BasicDataStructure-Assignment/Assignment1Q7.java
+++ BasicDataStructure-Assignment/Assignment1Q7.java
@@ -11,7 +11,7 @@ class SearchArray{
 public class Assignment1Q7 {
     public static void main(String[] args) {
         int arr[] = { 5,12,14,6,78,19,1,23,26,35,37,7,52,86,47};
-        int valueToCheck = 17;
+        int valueToCheck = 19;
         SearchArray obj=new SearchArray();
         if(obj.searchArray(arr,valueToCheck)){
             System.out.println("Value is present in the given array");
