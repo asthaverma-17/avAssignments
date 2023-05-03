af9bf1c1ce62251dade6627664da05cc65e0e47d
asthaverma-17 <av.asthaverma17@gmail.com>
2023-05-03 19:14:00 +0530

    assignment1Q8 is done

---
+++ BasicDataStructure-Assignment/Assignment1Q8.java
@@ -0,0 +1,27 @@
+class BubbleSort{
+
+    public int[] bubbleSort(int arr[]) {
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    return arr;
+    }
+}
+public class Assignment1Q8 {
+    public static void main(String[] args) {
+        int arr[] = {5,12,14,6,78,19,1,23,26,35,37,7,52,86,47};
+        BubbleSort bubbleObj=new BubbleSort();
+        int[] sortArr=bubbleObj.bubbleSort(arr);
+        for(int i=0;i<sortArr.length;i++){
+            System.out.print(sortArr[i]+" ");
+        }
+
+
+    }
+}
